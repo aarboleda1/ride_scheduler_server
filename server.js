daeb2e279c213634ce3f0825255e4fff49ad78b5
Anton <anton.arboleda@gmail.com>
2017-04-06 17:51:28 -0700

    [update] - figure out redirect and authentication into app

--- server.js
+++ server.js
@@ -12,17 +12,28 @@ var keys = require('./keys.js');
   Cron in order to which should scrape exisitng rides that will be scheduled 
 */
 
-var cron = schedule.scheduleJob('39 * * * * *', function() {
-  var url = `https://api.lyft.com/oauth/authorize?client_id=${keys.clientId}cope=public%20profile%20rides.read%20rides.request%20offline&state=<state_string>&response_type=code`;
-  console.log(keys.clientId);
-  fetch(url)
-  .then(res => res.json())
-  .then(json => console.log(json));
-});
+// var cron = schedule.scheduleJob('39 * * * * *', function() {
+//   var url = `https://api.lyft.com/oauth/authorize?client_id=${keys.clientId}cope=public%20profile%20rides.read%20rides.request%20offline&state=<state_string>&response_type=code`;
+//   console.log(keys.clientId);
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+// });
+app
+  .get( '/login', function( req, res ) {
+    var url = 'https://api.lyft.com/oauth/authorize?client_id=kK1f4CSvQe4r&scope=public%20profile%20rides.read%20rides.request%20offline&state=<state_string>&response_type=code';
+    fetch(url)
+      .then((response) => {
+        console.log(response)
+        res.redirect(response.url);
+      });
+  });
+
 
 app
   .get( '/', function( req, res ) {
-    res.sendFile( pat.join( __dirname, 'index.html' ));
+    // res.sendFile( pat.join( __dirname, 'index.html' ));
+    console.log('yoyo')
   });
 
 app.listen(3000, function () {
