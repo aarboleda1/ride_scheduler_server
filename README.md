1fea62186a021c5c660f487c9f05be282e5ea444
Anton <anton.arboleda@gmail.com>
2017-04-06 17:01:00 -0700

    [update] - code cleanup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ride_scheduler_server
+
+Todo:
+Write authentication in Node to authenticate users
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var schedule = require('node-schedule');
+var fetch = require('node-fetch');
+var pat = require('path');
+var keys = require('./keys.js');
+
+
+
+/*
+  Cron in order to which should scrape exisitng rides that will be scheduled 
+*/
+
+var cron = schedule.scheduleJob('39 * * * * *', function() {
+  var url = `https://api.lyft.com/oauth/authorize?client_id=${keys.clientId}cope=public%20profile%20rides.read%20rides.request%20offline&state=<state_string>&response_type=code`;
+  console.log(keys.clientId);
+  fetch(url)
+  .then(res => res.json())
+  .then(json => console.log(json));
 });
+
+app
+  .get( '/', function( req, res ) {
+    res.sendFile( pat.join( __dirname, 'index.html' ));
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
